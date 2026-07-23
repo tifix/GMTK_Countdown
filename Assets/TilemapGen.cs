2b4eaa0bd603d0f8d730010d77540b9f269285a5
Sorenon <sorenonandstuffs@gmail.com>
2026-07-23 17:33:47 +0100

    Initlial timemap experiments

---
+++ Assets/TilemapGen.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System;
+using UnityEngine.Tilemaps;
+
+
+public class TilemapGen : MonoBehaviour
+{
+
+    public GameObject debugSprite;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        //Tilemap tilemap = GetComponent<Tilemap>();
+        //tilemap.SetTile(new TileChangeData());
+
+
+        //Debug.Log("agony");
+
+        //int width = 110;
+        //int height = 100;
+
+        //Texture2D tex = new Texture2D(width, height);
+        ////Sprite sprite = new Sprite(tex);
+
+        //for (int x = 0; x < width; x++)
+        //{
+        //    for (int y = 0; y < height; y++)
+        //    {
+        //        tex.SetPixel(x, y, Color.red);
+        //    }
+        //}
+
+        //RawImage image = debugSprite.GetComponent<RawImage>();
+        //image.texture = tex;
+        //Debug.Log(tex);
+
+
+
+        //Mathf.PerlinNoise()        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/wtf.cs
@@ -0,0 +1,78 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class wtf : MonoBehaviour
+{
+    private Texture2D texture;
+    public float scale = 0.5f;
+    public float cutoff = 0.5f;
+
+    public float a = 3;
+    public float b = 2;
+    public float c = 0.5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int width = 100;
+        int height = 100;
+        texture = new Texture2D(width, height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int width = 100;
+        int height = 100;
+
+        float[] map = new float[width * height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float xDist = x - 50;
+                float yDist = (y - 50) * 1.3f;
+
+                float dist = Mathf.Sqrt(xDist * xDist + yDist * yDist);
+                float distMod = Mathf.Pow(dist / 50, a) * b - c;
+
+                map[x + height * y] = Mathf.PerlinNoise(x * scale, y * scale) + distMod;
+            }
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (map[x + height * y] > cutoff)
+                {
+                    texture.SetPixel(x, y, Color.rosyBrown);
+                    if (y < height - 1 && map[x + height * (y + 1)] < cutoff)
+                    {
+                        texture.SetPixel(x, y, Color.red);
+                    }
+                    else if (y > 0 && map[x + height * (y - 1)] < cutoff)
+                    {
+                        texture.SetPixel(x, y, Color.red);
+                    }
+
+                }
+                else
+                {
+                    texture.SetPixel(x, y, Color.black);
+                }
+                //texture.SetPixel(x, y, new Color(map[x + height * y], 0f, 0f));
+
+                //texture.SetPixel(x, y, new Color(0, Mathf.Pow(dist / 50, 3), 0));
+            }
+        }
+
+        texture.Apply();
+
+        RawImage image = GetComponent<RawImage>();
+        image.texture = (Texture)texture;
+    }
+}
